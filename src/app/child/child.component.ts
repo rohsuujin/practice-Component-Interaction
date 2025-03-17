1f834b3eca4813ba7741fc74e9186d7b7895de8a
rohsuujin <rohsuujin@gmail.com>
2025-03-17 10:53:11 -0500

    created input binding

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+  @Input() inputText: string = '';
 
+  @Output() outputValue: EventEmitter<string> = new EventEmitter<string>();
+
+  message: string = '';
+
+  sendMessage(text: string) {
+    this.message = text;
+    this.outputValue.emit(this.message);
+  }
 }
